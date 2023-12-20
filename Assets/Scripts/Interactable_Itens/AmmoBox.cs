bff788468394f90f9c9c65c48eef0adec7c0d8e6
João Prado <jpdiasprado01@gmail.com>
2023-12-20 14:20:26 -0300

    [WIP] - adicionado caixa de munição

--- Assets/Scripts/Interactable_Itens/AmmoBox.cs
+++ Assets/Scripts/Interactable_Itens/AmmoBox.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class AmmoBox : InteractableItem
 {
+    [SerializeField]
+    private int _ammoAmount;
     public override void InteractWithItem(Collider2D collision)
     {
 
+        collision.gameObject.GetComponentInChildren<PlayerAttack>().CurrentWeapon.MaxWeaponAmmo += _ammoAmount;
+        ItemInteracted();
     }
 
     public override void ItemInteracted()
     {
-
+        Destroy(gameObject);
     }
 }
+
