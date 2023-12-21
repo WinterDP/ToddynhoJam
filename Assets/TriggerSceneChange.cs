11f98724632f33c82eafc9b1c9faf234cd1c62a9
João Prado <jpdiasprado01@gmail.com>
2023-12-20 23:45:41 -0300

    [FEAT] - trocar cena trigger

---
+++ Assets/TriggerSceneChange.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TriggerSceneChange : MonoBehaviour
+{
+    public string nextScene;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
